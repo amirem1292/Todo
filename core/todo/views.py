1fdf3b7f1ee4b458aecfbc63f36e864121743ed8
amirem1292 <emami2979@gmail.com>
2025-12-10 17:14:11 +0330

    Revert "for delete"
    This reverts commit 2b2435d95847d947addc3c61caefad40cdf0f3d5.

--- core/todo/views.py
+++ core/todo/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView, FormView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import *
 from .forms import *
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -36,10 +36,14 @@ class TaskUpdateView(LoginRequiredMixin, UpdateView):
     template_name = 'todo/task_update.html'
 
 # delete tasks
-def task_delete_view(request, pk):
-    task = Task.objects.get(pk=pk)
-    task.delete()
-    return redirect('todo:task-list')
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    model = Task
+    success_url = reverse_lazy('todo:task-list')
+    # show the name of task
+    def get_context_data(self, **kwargs):
+        context = super(TaskDeleteView, self).get_context_data(**kwargs)
+        context['task'] = Task.objects.get(pk=self.kwargs['pk'])
+        return context
 # there was a problem with accounts urls so, I used fbv
 # login view
 def login_view(request):
@@ -61,4 +65,3 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('todo:task-list')
-
