72684a2eb447822f9a609ae15e2aaf9dbb3c4898
amirem1292 <emami2979@gmail.com>
2026-02-18 15:24:22 +0330

    view tests

--- core/todo/tests.py
+++ core/todo/tests.py
@@ -1,8 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, client, Client
+from django.urls import reverse
 
 from todo.forms import LoginForm, TaskUpdateForm
 from django.contrib.auth.models import User
 from .models import Task
+from .views import TaskListView
 
 # Create your tests here.
 
@@ -15,6 +17,10 @@ class FormTests(TestCase):
         self.assertTrue(form.is_valid())
 
     def test_login_form_with_valid_data(self):
+        form = LoginForm(data={"username": "admin", "password": "admin"})
+        self.assertTrue(form.is_valid())
+
+    def test_login_form_with_invalid_data(self):
         form = LoginForm(data={"username": "admin", "password": "admin"})
         self.assertTrue(form.is_valid())
 
@@ -31,3 +37,22 @@ class ModelTests(TestCase):
             is_done=False,
         )
         self.assertAlmostEqual(task.is_done, False)
+
+# views
+
+class ViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username="admin", password="admin")
+
+    def test_task_list(self ):
+        self.client.force_login(self.user)
+        url = reverse("todo:task-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_task_create(self):
+        self.client.force_login(self.user)
+        url = reverse("todo:task-create")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
